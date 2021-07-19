e2d6e4f1c2a0cb86ce8b480caafa3deb6c70d998
Ollie Copping <oliver.copping@diamond.ac.uk>
2021-07-19 17:16:55 +0100

    [WIP] Added test code for basic seaborn plot; Added chart.js files and test plot code; Created link to plots page in the side navbar

--- webapp/blueprints/plots/plots.py
+++ webapp/blueprints/plots/plots.py
@@ -1,4 +1,9 @@
-from base64 import b64encode
+import io
+import base64
+from io import BytesIO
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import matplotlib.pyplot as plt
+import seaborn as sns
 from datetime import datetime
 from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
 from flask import current_app as app
@@ -10,6 +15,19 @@ def show_plots():
 
     app.logger.info('-> plots')
 
-    # any variable options selected with code?
+    df = app.cham.df.to_json()
 
-    return render_template('/plots/plots.html.j2')
+    img = BytesIO()
+    sns.set_style("dark") #E.G.
+
+    y = [1,2,3,4,5]
+    x = [0,2,1,3,4]
+
+    plt.plot(x,y)
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return render_template('/plots/plots.html.j2', cham_df=df, plot_url=plot_url)
