57c62798bb0ee4d8a8300f4412ad0998bcda477b
Ollie Copping <oliver.copping@diamond.ac.uk>
2021-08-10 17:01:28 +0100

    [WIP] Started implementing 'trends' page features (i.e. chart generation based on selected data types); formtting changes and other minor changes

--- webapp/blueprints/trends/trends.py
+++ webapp/blueprints/trends/trends.py
@@ -1,33 +1,16 @@
-import io
-import base64
-from io import BytesIO
-#from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-#import matplotlib.pyplot as plt
-#import seaborn as sns
-from datetime import datetime
-from flask import Blueprint, render_template, request, redirect, session, url_for, jsonify
+import pandas as pd
+from flask import Blueprint
 from flask import current_app as app
+from flask import render_template, url_for
 
 from . import blueprint
+
 
 @blueprint.route('/trends', methods=['GET', 'POST'])
 def show_trends():
 
-    # app.logger.info('-> plots')
+    app.logger.info('-> trends')
 
-    # df = app.cham.df.to_json()
+    df = app.cham.to_json(orient='columns', date_format='iso')
 
-    # img = BytesIO()
-    # sns.set_style("dark") #E.G.
-
-    # y = [1,2,3,4,5]
-    # x = [0,2,1,3,4]
-
-    # plt.plot(x,y)
-    # plt.savefig(img, format='png')
-    # plt.close()
-    # img.seek(0)
-
-    # plot_url = base64.b64encode(img.getvalue()).decode()
-
-    return render_template('/trends/trends.html.j2')
+    return render_template('/trends/trends.html.j2', cham_df=df)
