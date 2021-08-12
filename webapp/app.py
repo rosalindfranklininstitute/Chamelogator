826b61a0b759a517e5bb8c48962e89a93ac23bcf
Ollie Copping <oliver.copping@diamond.ac.uk>
2021-08-12 16:48:54 +0100

    Added more columns to the dataframe making more data aviable to the webapp to be displayed (involved adding to and creating more tables for sqlalchemy)

--- webapp/app.py
+++ webapp/app.py
@@ -22,8 +22,9 @@ from . import models
 from .blueprints.compare import compare
 from .blueprints.data import data
 from .blueprints.trends import trends
-from .models.datadb import (Dispenses, Grids, Operations, Plunges,
-                            SampleDetails, Sessions, Treatments, db)
+from .models.datadb import (AcceptedGrids, Dispenses, GlowDischarges, Grids,
+                            Operations, Plunges, RejectedGrids, SampleDetails,
+                            Sessions, Treatments, db)
 
 # Fetch the bind to be used for queries
 bind = db.session.bind
@@ -36,6 +37,9 @@ plunges_query = db.session.query(Plunges)
 treatments_query = db.session.query(Treatments)
 dispenses_query = db.session.query(Dispenses)
 grids_query = db.session.query(Grids)
+accepted_grids_query = db.session.query(AcceptedGrids)
+rejected_grids_query = db.session.query(RejectedGrids)
+glow_discharges_query = db.session.query(GlowDischarges)
 
 # Use the statements of the queries to
 sessions = pd.read_sql_query(sessions_query.statement, bind)
@@ -45,6 +49,10 @@ plunges = pd.read_sql_query(plunges_query.statement, bind)
 treatments = pd.read_sql_query(treatments_query.statement, bind)
 dispenses = pd.read_sql_query(dispenses_query.statement, bind)
 grids = pd.read_sql_query(grids_query.statement, bind)
+accepted_grids = pd.read_sql_query(accepted_grids_query.statement, bind)
+rejected_grids = pd.read_sql_query(rejected_grids_query.statement, bind)
+glow_discharges = pd.read_sql_query(glow_discharges_query.statement, bind)
+
 
 # Merge the dataframes together using the common keys
 a = pd.merge(
@@ -61,26 +69,39 @@ c = pd.merge(b, grids, "outer", left_on="GridId", right_on="ROWID")
 d = pd.merge(c, plunges, "outer", left_on="ROWID_y", right_on="OperationId")
 e = pd.merge(d, treatments, "outer", left_on="GridId", right_on="GridId")
 f = pd.merge(e, dispenses, "outer", left_on="ROWID_y", right_on="OperationId")
+g = pd.merge(f, accepted_grids, "left", left_on="AcceptedId", right_on="ROWID", suffixes=("_acceptedx", "_acceptedy"))
+h = pd.merge(g, rejected_grids, "left", left_on="RejectedId", right_on="ROWID", suffixes=("_rejectedx", "_rejectedy"))
+j = pd.merge(h, glow_discharges, "outer", left_on="GlowDischargeId", right_on="ROWID", suffixes=("_glowx", "_glowy"))
+
+df = j
 
 # Declare and remove duplicate columns, and renaming the remaining ones to
 # more useful names
-labels = ["SessionId", "OperationId_x", "OperationId_y", "ROWID"]
+labels = ["SessionId", "OperationId_x", "OperationId_y", "ROWID_acceptedx", "ROWID_acceptedy", "ROWID_glowx", "GlowDischargeId"]
 app.logger.info(f"dropping: {labels}")
-f.drop(labels, axis=1, inplace=True)
-f.rename(
+df.drop(labels, axis=1, inplace=True)
+df.rename(
     columns={
         "ROWID_x": "SessionId",
         "ROWID_y": "OperationId",
         "DateTimeUTC_x": "SessionTime",
         "DateTimeUTC_y": "OperationTime",
+        "Notes_x": "SampleNotes",
+        "Notes_y": "GridNotes",
+        "Evaluation_rejectedx": "AcceptedEvaluation",
+        "Evaluation_rejectedy": "RejectedEvaluation",
+        "Comments_rejectedx": "AcceptedComments",
+        "Comments_rejectedy": "RejectedComments",
+        "ROWID_glowy": "GlowDischargeId"
     },
     inplace=True,
 )
 
 # Sort the new massive dataframe be ascending Session IDs
-f.sort_values(by=["SessionId"], ascending=True, inplace=True)
+df.sort_values(by=["SessionId"], ascending=True, inplace=True)
 # If there isn't a value in the SessionId column, it probably is junk
-df = f.dropna(subset=["SessionId"])
+df.dropna(subset=["SessionId"], inplace=True)
+app.logger.info(list(df.columns.values))
 
 # Store df in app namespace for easy access by rest of app
 # (should I do this?)
--- webapp/models/datadb.py
+++ webapp/models/datadb.py
@@ -1,43 +1,74 @@
 #from sqlalchemy.ext.associationproxy import association_proxy
 
+from sqlalchemy.orm import with_expression
+
 from . import db
+
 
 class Sessions(db.Model):
     __tablename__ = 'Sessions'
 
     ROWID = db.Column(db.Integer, primary_key=True)
     DateTimeUTC = db.Column(db.DateTime)
+    Operator = db.Column(db.Text)
     Project = db.Column(db.Text)
+    WorkFlowMode = db.Column(db.Integer)
 
-    def __init__(self, session_id, date_time_utc, project='null'):
+    def __init__(self, session_id, date_time_utc, operator, work_flow_mode, project='null'):
         self.ROWID = session_id
         self.DateTimeUTC = date_time_utc
+        self.Operator = operator
         self.Project = project
+        self.WorkFlowMode = work_flow_mode
 
 
 class SampleDetails(db.Model):
     __tablename__ = 'SampleDetails'
 
     SessionId = db.Column(db.Integer, db.ForeignKey('Sessions.ROWID'), primary_key=True)
+    Name = db.Column(db.Text)
     ProteinClass = db.Column(db.Text)
     MolecularWeight = db.Column(db.Integer)
     Concentration = db.Column(db.Float)
     BufferDescription = db.Column(db.Text)
     BufferConcentration = db.Column(db.Integer)
+    BufferUnit = db.Column(db.Integer)
+    PH = db.Column(db.Float)
     DetergentType = db.Column(db.Text)
     DetergentConcentration = db.Column(db.Float)
+    DetergentUnit = db.Column(db.Integer)
+    SaltType = db.Column(db.Text)
+    SaltConcentration = db.Column(db.Float)
+    SaltUnit = db.Column(db.Integer)
+    OtherType = db.Column(db.Integer)
     GlycerolConcentration = db.Column(db.Integer)
+    GlycerolUnit = db.Column(db.Integer)
+    AspirateVolume = db.Column(db.Float)
+    SampleBlockTemperature = db.Column(db.Float)
+    Notes = db.Column(db.Text)
 
-    def __init__(self, session_id, protein_class, molecular_weight, concentration, buffer_desc, buffer_conc, detergent_type, detergent_conc, glycerol_conc):
+    def __init__(self, session_id, name, protein_class, molecular_weight, concentration, buffer_desc, buffer_conc, buffer_unit, ph, detergent_type, detergent_conc, detergent_unit, salt_type, salt_conc, salt_unit, other_type, glycerol_conc, glycerol_unit, aspirate_vol, sample_block_temp, notes):
         self.SessionId = session_id
+        self.Name = name
         self.ProteinClass = protein_class
         self.MolecularWeight = molecular_weight
         self.Concentration = concentration
         self.BufferDescription = buffer_desc
         self.BufferConcentration = buffer_conc
+        self.BufferUnit = buffer_unit
+        self.PH = ph
         self.DetergentType = detergent_type
         self.DetergentConcentration = detergent_conc
+        self.DetergentUnit = detergent_unit
+        self.SaltType = salt_type
+        self.SaltConcentration = salt_conc
+        self.SaltUnit = salt_unit
+        self.OtherType = other_type
         self.GlycerolConcentration = glycerol_conc
+        self.GlycerolUnit = glycerol_unit
+        self.AspirateVolume = aspirate_vol
+        self.SampleBlockTemperature = sample_block_temp
+        self.Notes = notes
 
 
 class Operations(db.Model):
@@ -47,25 +78,35 @@ class Operations(db.Model):
     DateTimeUTC = db.Column(db.DateTime)
     GridId = db.Column(db.Integer, primary_key=True)
     SampleDetailsId = db.Column(db.Integer, db.ForeignKey('SampleDetails.SessionId'))
+    AcceptedId = db.Column(db.Integer, primary_key=True)
+    RejectedId = db.Column(db.Integer, primary_key=True)
 
-    def __init__(self, operation_id, operation_time, grid_id, sample_details_id):
+    def __init__(self, operation_id, operation_time, grid_id, sample_details_id, accepted_id, rejected_id):
         self.ROWID = operation_id
         self.DateTimeUTC = operation_time
         self.GridId = grid_id
         self.SampleDetailsId = sample_details_id
+        self.AcceptedId = accepted_id
+        self.RejectedId = rejected_id        
 
 
 class Plunges(db.Model):
     __tablename__ = 'Plunges'
 
     OperationId = db.Column(db.Integer, db.ForeignKey('Operations.ROWID'), primary_key=True)
+    EthaneTemp = db.Column(db.Float)
+    LN2Level = db.Column(db.Integer)
     WickingTimeUsed = db.Column(db.Time)
     Mode = db.Column(db.Integer)
+    WickingTimeSuggested = db.Column(db.Text)
 
-    def __init__(self, operation_id, wicking_time_used, plunge_mode):
+    def __init__(self, operation_id, ethane_temp, ln2_level, wicking_time_used, plunge_mode, wicking_time_suggested):
         self.OperationId = operation_id
+        self.EthaneTemp = ethane_temp
+        self.LN2Level = ln2_level
         self.WickingTimeUsed = wicking_time_used
         self.Mode = plunge_mode
+        self.WickingTimeSuggested = wicking_time_suggested
 
 
 class Treatments(db.Model):
@@ -83,10 +124,22 @@ class Dispenses(db.Model):
     __tablename__ = 'Dispenses'
 
     OperationId = db.Column(db.Integer, db.ForeignKey('Operations.ROWID'), primary_key=True)
+    AmbientRH = db.Column(db.Float)
+    AmbientTemp = db.Column(db.Float)
+    ShroudRH = db.Column(db.Float)
+    ShroudTemp = db.Column(db.Float)
+    RemainingVolume = db.Column(db.Float)
+    DispenserAmplitude = db.Column(db.Integer)
     SampleTimer = db.Column(db.Time)
 
-    def __init__(self, operation_id, sample_timer):
+    def __init__(self, operation_id, ambient_rh, ambient_temp, shroud_rh, shroud_temp, remaining_vol, dispenser_amp, sample_timer):
         self.OperationId = operation_id
+        self.AmbientRH = ambient_rh
+        self.AmbientTemp = ambient_temp
+        self.ShroudRH = shroud_rh
+        self.ShroudTemp = shroud_temp
+        self.RemainingVolume = remaining_vol
+        self.DispenserAmplitude = dispenser_amp
         self.SampleTimer = sample_timer
 
 
@@ -94,9 +147,58 @@ class Grids(db.Model):
     __tablename__ = 'Grids'
 
     ROWID = db.Column(db.Integer, db.ForeignKey('Operations.GridId'), primary_key=True)
+    Box = db.Column(db.Text)
+    Slot = db.Column(db.Integer)
+    Type = db.Column(db.Text)
     SerialNumber = db.Column(db.Text)
+    BatchNumber = db.Column(db.Text)
+    Notes = db.Column(db.Text)
 
-    def __init__(self, grid_id, serial_number):
+    def __init__(self, grid_id, box, slot, type, serial_number, batch_number, notes):
         self.ROWID = grid_id
+        self.Box = box
+        self.Slot = slot
+        self.Type = type
         self.SerialNumber = serial_number
+        self.BatchNumber = batch_number
+        self.Notes = notes
 
+
+class AcceptedGrids(db.Model):
+    __tablename__ = 'AcceptedGrids'
+
+    ROWID = db.Column(db.Integer, db.ForeignKey('Operations.AcceptedId'), primary_key=True)
+    Evaluation = db.Column(db.Integer)
+    WithGradient = db.Column(db.Integer)
+    Comments = db.Column(db.Text)
+
+    def __init__(self, accepted_id, evaluation, with_gradient, comments):
+        self.ROWID = accepted_id
+        self.Evaluation = evaluation
+        self.WithGradient = with_gradient
+        self.Comments = comments
+
+
+class GlowDischarges(db.Model):
+    __tablename__ = 'GlowDischarges'
+
+    ROWID = db.Column(db.Integer, db.ForeignKey('Treatments.GlowDischargeId'), primary_key=True)
+    Instrument = db.Column(db.Text)
+    Duration = db.Column(db.Time)
+
+    def __init__(self, glow_discharge_id, instrument, duration):
+        self.ROWID = glow_discharge_id
+        self.Instrument = instrument
+        self.Duration = duration
+
+class RejectedGrids(db.Model):
+    __tablename__ = 'RejectedGrids'
+
+    ROWID = db.Column(db.Integer, db.ForeignKey('Operations.RejectedId'), primary_key=True)
+    Evaluation = db.Column(db.Integer)
+    Comments = db.Column(db.Text)
+
+    def __init__(self, rejected_id, evaluation, with_gradient, comments):
+        self.ROWID = rejected_id
+        self.Evaluation = evaluation
+        self.Comments = comments
